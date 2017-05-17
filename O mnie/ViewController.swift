d502bd14ecc4f5ffaa3f8ab648da3f8c242fc281
Bartosz Machowski <bartosz.machowski@gmail.com>
2017-05-17 21:32:17 +0200

    Added MeView & View ctrl

--- O mnie/ViewController.swift
+++ O mnie/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func strzalkaIntro(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let kolorTla = UIColor(red: 0/255.0, green: 66/255.0, blue: 132/255.0, alpha: 1.0)
+        view.backgroundColor = kolorTla
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
